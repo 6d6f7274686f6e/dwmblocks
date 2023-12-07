2c7dead2b7b8e37be6bb78cc6291e07d0312ae47
6d6f7274686f6e <4648531+6d6f7274686f6e@users.noreply.github.com>
2023-12-07 10:32:24 +0100

    small fix

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 static const Block blocks[] = {
   /*Icon*/  /*Command*/                               /*Update Interval*/ /*Update Signal*/
-  {"",     "/usr/local/bin/scripts/dwm/bar/music",       10,                 11},
+  {"",     "/usr/local/bin/scripts/dwm/bar/music",       5,                  11},
   {"  ",  "/usr/local/bin/scripts/dwm/bar/volume",      60,                 12},
-  {" ⚡ ", "/usr/local/bin/scripts/dwm/bar/battery",     10,                 13},
-  {"  ",  "/usr/local/bin/scripts/dwm/bar/temperature", 10,                 14},
+  {" ⚡ ", "/usr/local/bin/scripts/dwm/bar/battery",     5,                  13},
+  {"  ",  "/usr/local/bin/scripts/dwm/bar/temperature", 5,                  14},
   {"  ",  "/usr/local/bin/scripts/dwm/bar/calendar",    600,                15},
   {"  ",  "/usr/local/bin/scripts/dwm/bar/time",        1,                  16},
 };
@@ -11,4 +11,4 @@ static const Block blocks[] = {
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char *delim = " |";
 static char *adminscriptspath = "/usr/local/sbin/scripts";
-static char *dmwscriptspath   = "/usr/local/bin/scripts/dwm";
+static char *dwmscriptspath   = "/usr/local/bin/scripts/dwm";
